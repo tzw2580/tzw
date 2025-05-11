b6e33edec961f9879af7b13569a15823e18c4a22
tzw <tzw2580@163.cm>
2025-05-11 15:44:10 +0800

    Python 100练习题

--- practice/tm3.py
+++ practice/tm3.py
@@ -1,19 +1,7 @@
-# 题目3：一个整数，它加上 100后是一个完全平方数，再加上268 又是一个完全平方数，请问该数是多少?
-# 解方程的思想
+# 题目3：一个整数，它加上 100和加上268都是一个完全平方数，请问该数是多少?
+import math
+# 方法一：解方程的思想
 def find_num():
-    for m in range(1,10000):
-        x=m**2-100
-        n_squared=x+268
-        n=int(math.sqrt(n_squared))
-        if n**2 == n_squared:
-            return x
-    return None
-result=find_num()
-print(result)
-
-# 以上方法，找到第一个满足条件的x就返回-->-99
-# 以下方法，找到所有满足条件的整数-->[-99, 21, 261, 1581]
-def findall_num():
     lst=[]
     for m in range(1,10000):
         x=m**2-100
@@ -22,7 +10,21 @@ def findall_num():
         if n**2 == n_squared:
             lst.append(x)
     return lst
-result=findall_num()
+
+result=find_num()
 print(result)
+
+
+# 方法二：
+for i in range(10000):
+    m=math.sqrt(i+100)
+    n=math.sqrt(i+268)
+    if m%1 == 0 and n%1 == 0: #判断是不是整数
+        print(i)
+
+
+
+
+
 
 
