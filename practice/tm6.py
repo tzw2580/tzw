ebd53e42061c9174f7a95ff81009c751c2da773a
tzw <tzw2580@163.cm>
2025-05-13 20:04:50 +0800

    Python 100练习题

--- practice/tm6.py
+++ practice/tm6.py
@@ -16,5 +16,14 @@ for i in range(dep-2):
     x,y=y,x+y
     lst1.append(y)
 print(lst1)
+# 方法三：递归
+def fun(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fun(n-1)+fun(n-2)
+
+for i in range(1,11):
+    print(fun(i))
 
 
